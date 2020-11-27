49082bfaa74d8d3afa0066bd488d227d758382f3
Henrique Goncalves <henriquesg09@hotmail.com>
2020-11-27 18:15:56 -0300

    Feature(dashboard-structure): Dashboard structure

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -4,6 +4,8 @@
 module.exports = ({
   primary: '#F4CA28',
   primaryDarker: '#F2B619',
+  lightBlue: '#5765F7',
+  purple: '#AF3CBA',
   white: '#FFFFFF',
   black: '#111111',
   blackSoft: '#161616',
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useAuth } from 'contexts/AuthProvider';
 
+import logo from 'assets/logo/set-logo.svg';
+
+import {
+  Container,
+  HeaderWrapper,
+  HeaderContent,
+  HeaderLogo,
+  Profile,
+  ProfileInfo,
+  ProfileMenu,
+} from './Dashboard.style';
+
 const Dashboard = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const user = useAuth() || {};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
+  const handleLogout = () => {
     user.logout();
   };
 
   return (
-    <div>
-      <p>
-        Seja bem vindo
-        {' '}
-        <strong>
-          {user.currentUser?.displayName}
-        </strong>
-        <img src={user.currentUser?.photoURL} alt="Perfil" />
-      </p>
-      <form onSubmit={handleSubmit}>
-        <button type="submit">Log out</button>
-      </form>
-    </div>
+    <Container>
+      <HeaderWrapper>
+        <HeaderContent>
+          <HeaderLogo>
+            <img src={logo} alt="" />
+            <p>Set</p>
+          </HeaderLogo>
+
+          <Profile>
+            <ProfileInfo type="button" onClick={toggleMenu}>
+              <img src={user.currentUser?.photoURL} alt="" />
+              <p>{user.currentUser?.displayName.split(' ')[0]}</p>
+            </ProfileInfo>
+
+            <ProfileMenu visible={menuOpen}>
+              <button type="button" onClick={handleLogout}>
+                Sair
+              </button>
+            </ProfileMenu>
+          </Profile>
+        </HeaderContent>
+      </HeaderWrapper>
+    </Container>
   );
 };
 
